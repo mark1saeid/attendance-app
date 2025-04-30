d2d23cae3d849bcfa92112241e0f1cea2247696b
Mark Saeid <marksaeid77@gmail.com>
2025-05-01 02:36:14 +0300

    fix

--- lib/utils/date_convert.dart
+++ lib/utils/date_convert.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 String formatDateID(String pattern, String? dateStr) {
   if (dateStr == null) return "-";
   final date =
-      DateFormat(pattern, "en").format(DateTime.parse(dateStr).toLocal());
+      DateFormat(pattern, "en-SA").format(DateTime.parse(dateStr).toLocal());
 
   return date;
 }
@@ -15,7 +15,7 @@ String parseUtcToLocal({
 }) {
   if (date == null) return '-';
   final dateUtc = DateFormat(utcPattern).parse(date).toLocal();
-  final dateLocal = DateFormat(pattern, 'en').format(dateUtc);
+  final dateLocal = DateFormat(pattern, 'en-SA').format(dateUtc);
 
   return dateLocal;
 }
@@ -23,13 +23,13 @@ String parseUtcToLocal({
 String timeHm(String? time) {
   if (time == null) return '-';
   var dateTimeUtc = DateFormat.Hms().parse(time).toLocal();
-  var timeIn12 = DateFormat.jm().format(dateTimeUtc);
+  var timeIn12 = DateFormat.jm('en-SA').format(dateTimeUtc);
   return timeIn12.toString();
 }
 
 String timeParseHm(String? utcTime) {
   if (utcTime == null) return '-';
   var dateTimeUtc = DateFormat.Hms().parse(utcTime).toLocal();
-  var timeIn12 = DateFormat.jm().format(dateTimeUtc);
+  var timeIn12 = DateFormat.jm('en-SA').format(dateTimeUtc);
   return timeIn12.toString();
 }
